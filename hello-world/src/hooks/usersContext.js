fc9531c16bae0a52260c95d781e8b3bd6acbe40c
githinjisamson1 <githinjisamson148@gmail.com>
2024-03-20 05:34:53 +0300

    useReducer setup

--- hello-world/src/hooks/usersContext.js
+++ hello-world/src/hooks/usersContext.js
@@ -1,11 +1,63 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer } from "react";
 
-// retuurns Provider, Consumer
+// returns Provider, Consumer
 const UsersContext = React.createContext();
 
 // wrap return in Provider
 const UsersProvider = ({ children }) => {
-  return <UsersContext.Provider>{children}</UsersContext.Provider>;
+  // initialUsersState
+  const initialUsersState = {
+    loading: false,
+    users: [],
+    error: "",
+  };
+
+  //   usersReducer
+  const usersReducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_REQUEST":
+        return {
+          ...state,
+          loading: true,
+          users: [],
+          error: "",
+        };
+      case "FETCH_SUCCESS":
+        return {
+          ...state,
+          loading: false,
+          users: action.payload,
+          error: "",
+        };
+      case "FETCH_FAILURE":
+        return {
+          ...state,
+          loading: false,
+          users: [],
+          error: action.payload,
+        };
+
+      default:
+        return state;
+    }
+  };
+
+//   useReducer hook
+  const [usersState, dispatchForUsersState] = useReducer(
+    usersReducer,
+    initialUsersState
+  );
+
+  return (
+    <UsersContext.Provider
+      value={{
+        usersState,
+        dispatchForUsersState,
+      }}
+    >
+      {children}
+    </UsersContext.Provider>
+  );
 };
 
 const useGlobalUsersContext = () => {
