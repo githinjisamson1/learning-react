24ccaf42aeed3bf2505d5e0dfe1a5b5fe9ea1c8b
githinjisamson1 <githinjisamson148@gmail.com>
2024-03-20 05:27:17 +0300

    Wrap entire app in UsersProvider == provide UsersContext

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./styles.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { UsersProvider } from "./hooks/usersContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UsersProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UsersProvider>
   </>
 );
